41d4a970b6e7744c26f6bea2e6ce037e7095d65e
wx5331771 <18777815234@163.com>
2023-07-29 01:52:07 +0800

    添加db日志

--- source/db/drv_db.c
+++ source/db/drv_db.c
@@ -10,11 +10,51 @@ drv_db_t *get_db_ctrl(void)
     return &g_drv_db;
 }
 
+static int drv_db_add_history(void *data, int lenght)
+{
+    drv_db_t *ctx = get_db_ctrl();
+
+    int ret = mln_queue_full(ctx->history.queue);
+    if (ret != 0) {
+        void *old = mln_queue_get(ctx->history.queue);
+        mln_queue_remove(ctx->history.queue);
+        ctx->history.attr.free_handler(old);
+    }
+
+    void *new = malloc(sizeof(void) * lenght);
+    ERRP(new == NULL, "malloc error", goto ERR0);
+
+    memcpy(new, data, lenght);
+    mln_queue_append(ctx->history.queue, new);
+
+    return SUCCESS;
+ERR0:
+    return FAILURE;
+}
+
+static int drv_db_exec(const char *sql, int (*callback)(void*, int, char**, char**), void *call_param)
+{
+    drv_db_t *ctx = get_db_ctrl();
+    int ret = sqlite3_exec(ctx->db, sql, callback, call_param, &ctx->db_errmsg);
+    ERRP(ret != SQLITE_OK, ctx->db_errmsg, goto ERR0);
+    drv_db_add_history((void *)sql, strlen(sql) + 1);
+    return ret;
+ERR0:
+    sqlite3_free(ctx->db_errmsg);
+    return ret;
+}
+
 int drv_db_init(void)
 {
     drv_db_t *ctx = get_db_ctrl();
 
-    int ret = sqlite3_open_v2(SQLFILE_PATH, &ctx->db, SQLITE_OPEN_READWRITE, NULL);
+    ctx->db_path = SQLFILE_PATH;
+    ctx->history.attr.qlen = DB_HISTORY_MAX_LEN;
+    ctx->history.attr.free_handler = free;
+    ctx->history.queue = mln_queue_init(&ctx->history.attr);
+    ERRP(ctx->history.queue == NULL, "history init err", goto ERR0);
+
+    int ret = sqlite3_open_v2(ctx->db_path, &ctx->db, SQLITE_OPEN_READWRITE, NULL);
     if (ret != SQLITE_OK) {
         elog_e("debug", "数据库%s不存在, 初始化数据库!!!", SQLFILE_PATH);
         ret = sqlite3_open_v2(SQLFILE_PATH, &ctx->db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
@@ -23,7 +63,8 @@ int drv_db_init(void)
         ERRPGO(ret != SUCCESS, goto ERR0);
         elog_e("debug", "初始化数据库成功!!!");
     }
-    
+
+    drv_db_exec(SQL_INSERT_DEFAULT_USER, NULL, NULL);
 
     ctx->init = TRUE;
 
@@ -31,6 +72,18 @@ int drv_db_init(void)
 ERR0:
     return FAILURE;
 }
+
+// int pri(void *data, void *param)
+// {
+//     printf("%s\n", (char *)data);
+//     return 0;
+// }
+// static void debug_print_history()
+// {
+//     drv_db_t *ctx = get_db_ctrl();
+
+//     mln_queue_iterate(ctx->history.queue, pri, NULL);
+// }
 
 void drv_db_destroy(void)
 {
@@ -40,6 +93,9 @@ void drv_db_destroy(void)
         return;
     }
 
+    // debug_print_history();
+
+    mln_queue_destroy(ctx->history.queue);
     sqlite3_close_v2(ctx->db);
     ctx->db = NULL;
     ctx->init = FALSE;
@@ -78,8 +134,11 @@ ERR0:
 int drv_db_create_table(void)
 {
     drv_db_t *ctx = get_db_ctrl();
-    int ret = sqlite3_exec(ctx->db, SQL_CREATE_USER_TABLE, NULL, NULL, NULL);
+
+    int ret = drv_db_exec(SQL_CREATE_USER_TABLE, NULL, NULL);
     ERRP(ret != SQLITE_OK, "创建用户表失败！！！", goto ERR0);
+    ret = drv_db_exec(SQL_INSERT_DEFAULT_USER, NULL, NULL);
+    ERRP(ret != SQLITE_OK, "添加默认用户失败！！！", goto ERR0);
 
     return SUCCESS;
 ERR0:
--- source/db/drv_db.h
+++ source/db/drv_db.h
@@ -5,14 +5,22 @@
 #include "mln_queue.h"
 #include <sqlite3.h>
 
+struct drv_db_sql_history {
+    mln_queue_t *queue;
+    struct mln_queue_attr attr;
+};
+
 typedef struct drv_db {
     bool init;
     bool is_first_load;
+    char *db_path;
     sqlite3 *db;
-
+    char *db_errmsg;
+    struct drv_db_sql_history history;
 } drv_db_t;
 
 #define SQLFILE_PATH "db/car.db"
+#define DB_HISTORY_MAX_LEN  20 
 
 #define MAGIC_NUMBER                "car_magic"
 #define SQL_FIND_IS_HAS_TABLE_NAME  "SELECT COUNT(*) FROM sqlite_master where type ='table' and name = '%s'"
@@ -22,11 +30,15 @@ typedef struct drv_db {
 "CREATE TABLE user ( \
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
     username TEXT NOT NULL, \
-    pathconf TEXT NOT NULL, \
+    password TEXT NOT NULL, \
     limits INT NOT NULL, \
     login_time INTEGER NOT NULL, \
     create_time INTEGER NOT NULL \
 );"
+
+#define SQL_INSERT_DEFAULT_USER \
+"INSERT INTO user (id, username, password, limits, login_time, create_time) \
+    VALUES (NULL, 'admin', 'admin', 1, 10, 10);"
 
 
 int drv_db_init(void);
